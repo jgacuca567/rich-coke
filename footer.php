1a221ccc20ff24935cf27e7dd292ec91af49f369
JamesVanWaza <jgacuca@gmail.com>
2017-01-07 14:41:29 -0500

    Added the Footer

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
+	<footer>
+	    Office Location: <br> 
+	    Maintained by <a href="https://dosbranding.com" target="_blank">DosBranding</a>
+	</footer>
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="node_modules/moment/moment.js"></script>
+	<script src="node_modules/slick-carousel/slick/slick.js"></script>
 </body>
 </html>
