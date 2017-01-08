860c6a653059507b41c6ebc71d2a76eb8a43de16
JamesVanWaza <jgacuca@gmail.com>
2017-01-08 11:33:51 -0500

    Updated the Navbar

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <?php include_once 'html5req.php';?>
-    <nav class="top-bar" data-topbar role="navigation" data-options="is_hover: false">
+    <nav class="navbar" role="navigation">
         <ul class="title-area">
             <li class="name">
-                <h1><a href="index.php">Estate</a></h1>
+                <h1><a href="index.php">RICH</a></h1>
             </li>
             <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
             <li class="toggle-topbar menu-icon"><a href="#"><span>MENU</span></a>
@@ -11,10 +11,13 @@
     <section class="top-bar-section">
         <!-- Right Nav Section -->
         <ul class="right">
-            <li><a href="about.php" title="About Us">About Us</a></li>
-            <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
-            <li><a href="contact.php" title="Contact us">Contact Us</a></li>
-            <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
+            <li><a href="about.php" title="About Us">Rich Profile</a></li>
+            <li><a href="faqs.php" title="Frequently Asked Questions">Rich Freebies</a></li>
+            <li><a href="contact.php" title="Contact us">Rich Tools</a></li>
+            <li><a href="index.php" title="Return to Home Page">Rich data</a></li>
+            <li><a href="index.php" title="Return to Home Page">Online shop</a></li>
+            <li><a href="index.php" title="Return to Home Page">My Account</a></li>
+            <li><a href="index.php" title="Return to Home Page">Register</a></li>
         </ul>
     </section>
 </nav>
