1780cf0d625c7edc63cf5751c3484530413b05e9
JamesVanWaza <jgacuca@gmail.com>
2017-01-08 11:34:59 -0500

    Included the prelayout of the page

--- index2.php
+++ index2.php
@@ -1,3 +1,51 @@
 <?php include 'nav.php';?>
+<article class="single-item">
+	<div>
+		<img src="https://source.unsplash.com/category/technology/1000x450" alt="">
+	</div>
+</article>
+<section>
+	<div>
+		<h2>WHAT'S NEW</h2>
+		Insert Whats new content
+	</div>
+	<div>
+		<h2>REUTERS INSIDER</h2>
+	   <iframe src="https://embed.insider.thomsonreuters.com/kwf/widget/caas/instanceid/0_1ik3a3sc/cid/0_wkhnlgkw/layout/no-playlist
+	<https://embed.insider.thomsonreuters.com/kwf/widget/caas/instanceid/0_1ik3a3sc/cid/0_wkhnlgkw/layout/no-playlist>" width="100%" height="100%" style="border:0" frameborder="0"
+	    allowfullscreen webkitallowfullscreen mozallowfullscreen>
+	  </iframe>
+	</div>
+
+	<div>
+		<h2>ON TWITTER</h2>
+	   	<a class="twitter-timeline" href="https://twitter.com/alykhansatchu">Tweets by alykhansatchu</a>
+	   	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+	</div>
+</section>
+<section>
+	<div>
+		<h3>REGISTER NOW</h3>
+		<h4>THE INTELLIGENT CHOICE</h4>
+		Subscribe today for a free account
+		<a href="#">Benefits</a>
+		<a href="#">Register Now</a>
+		<form action="">
+			<input type="email" placeholder="Email Address">
+			<input type="password" placeholder="Email Address">
+			<button>Login</button>
+		</form>
+		<a href="#">Forgot Your Password</a> -- Implement 2 Factor Authentication
+	</div>
+	<div>
+		<h3>RICH WRAP-UPS</h3>
+	</div>
+	<div>
+		<h3>RICH GRAPHS</h3>
+	</div>
+	<div>
+		<h3>COMMENTS</h3>
+	</div>
+</section>
 
 <?php include 'footer.php';
